d84a7697d5108c9e466201cf46789fbf2553dfdb
vliux_shuwen <liuxin@shuwen.com>
2017-07-08 16:56:40 +0800

    improve guide msg.

--- app/src/main/java/com/example/vliux/notificationlistener/SettingsActivity.java
+++ app/src/main/java/com/example/vliux/notificationlistener/SettingsActivity.java
@@ -1,19 +1,9 @@
 package com.example.vliux.notificationlistener;
 
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.support.annotation.Nullable;
-import android.support.v4.util.ArrayMap;
-
-import com.example.vliux.notificationlistener.util.Apps;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by vliux on 17/7/7.
@@ -24,17 +14,6 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.settings);
-        
-        final Preference pkgPref = findPreference(getString(R.string.pref_pkg_id));
-        pkgPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                return true;
-            }
-        });
-        final Preference sysNotifPref = findPreference(getString(R.string.pref_sys_notif_id));
-        sysNotifPref.setOnPreferenceChangeListener(mPrefChangeedListener);
     }
     
     private final Preference.OnPreferenceChangeListener mPrefChangeedListener = new Preference.OnPreferenceChangeListener() {
